a01aa021a092cb4e50b20086fb1785b9101dcdb3
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-17 12:57:42 -0800

    styled divs

--- hackathon/src/components/FormField/FormField.jsx
+++ hackathon/src/components/FormField/FormField.jsx
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
-
+import "./FormField.scss";
 const FormField = () => {
-  // State to manage form data
   const [formData, setFormData] = useState({
-    // Business Information
     selling: "",
-    businessOperation: "",
-
-    // Geographic Information
+    businessOperation: "", // This state can be removed if not needed for dropdown
     city: "",
     province: "",
     country: "",
   });
 
-  // Handle input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -22,83 +17,101 @@ const FormField = () => {
     });
   };
 
-  // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    // You can perform further actions with the form data here
     console.log("Form submitted:", formData);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      {/* Business Information Section */}
-      <h1>Business Information</h1>
+      <div className="formWrapper">
+        <div className="border-control">
+          <h1 className="formTitle">Business Information</h1>
+        </div>
+        <div className="form-input-wrapper">
+          <label>
+            <div className="form-input-columnizer">
+              What are you selling:
+              <input
+                type="text"
+                name="selling"
+                value={formData.selling}
+                onChange={handleInputChange}
+              />
+            </div>
+          </label>
 
-      <label>
-        What are you selling:
-        <input
-          type="text"
-          name="selling"
-          value={formData.selling}
-          onChange={handleInputChange}
-        />
-      </label>
+          <br />
 
-      <br />
+          <label>
+            <div className="form-input-columnizer">
+              What kind of operation is your business:
+              {/* Replace the input with a select dropdown */}
+              <select
+                name="businessOperation"
+                value={formData.businessOperation}
+                onChange={handleInputChange}
+              >
+                <option value="">Select...</option>
+                <option value="distributor">Distributor</option>
+                <option value="manufacturing">Manufacturing</option>
+                {/* Add more options as needed */}
+              </select>
+            </div>
+          </label>
+        </div>
+        <br />
+        <div className="border-control">
+          <h1 className="formTitle">Geographic Information</h1>
+        </div>
+        <div className="form-input-wrapper">
+          <div className="form-input-row">
+            <label>
+              <div className="form-input-columnizer">
+                Where is your business located?
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="City"
+                  value={formData.city}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
 
-      <label>
-        What kind of operation is your business:
-        <input
-          type="text"
-          name="businessOperation"
-          value={formData.businessOperation}
-          onChange={handleInputChange}
-        />
-      </label>
+            <br />
 
-      <br />
+            <label>
+              <div className="form-input-columnizer">
+                Province/State:
+                <input
+                  type="text"
+                  name="province"
+                  value={formData.province}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+        </div>
+        <br />
+        <div className="form-input-wrapper">
+          <label>
+            <div className="form-input-columnizer">
+              Country:
+              <input
+                type="text"
+                name="country"
+                value={formData.country}
+                onChange={handleInputChange}
+              />
+            </div>
+          </label>
+        </div>
+        <br />
 
-      {/* Geographic Information Section */}
-      <h1>Geographic Information</h1>
-
-      <label>
-        Where is your business located?
-        <input
-          type="text"
-          name="city"
-          placeholder="City"
-          value={formData.city}
-          onChange={handleInputChange}
-        />
-      </label>
-
-      <br />
-
-      <label>
-        Province/State:
-        <input
-          type="text"
-          name="province"
-          value={formData.province}
-          onChange={handleInputChange}
-        />
-      </label>
-
-      <br />
-
-      <label>
-        Country:
-        <input
-          type="text"
-          name="country"
-          value={formData.country}
-          onChange={handleInputChange}
-        />
-      </label>
-
-      <br />
-
-      <button type="submit">Create Roadmap</button>
+        <button type="submit">Create Roadmap</button>
+      </div>
     </form>
   );
 };
