85888c63c9dd86af80a56bd78a8d83b9cca18480
Stephen Im <imssbb@gmail.com>
2023-11-16 22:54:06 -0800

    setup environment

---
+++ hackathon/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer/Footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__text">Hello</p>
+    </div>
+  );
+}
+
+export default Footer;
